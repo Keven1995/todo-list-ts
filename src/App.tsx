4d367ea21b6bcd88c1e86b8d72122e326a4ee008
Keven <lkeven77@gmail.com>
2024-07-25 12:35:01 -0300

    Adicionando funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Modal from "./Modal";
 
 function App() {
   const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
 
   const deleteTask = (id: number) => {
     setTaskList(
@@ -22,10 +23,43 @@ function App() {
     );
   };
 
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal");
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (task: ITask): void => {
+    hideOrShowModal(true);
+    setTaskToUpdate(task);
+  };
+
+  const updateTask = (id: number, title: string, difficulty: number) => {
+    const updatedTask: ITask = { id, title, difficulty };
+
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task;
+    });
+
+    setTaskList(updatedItems);
+
+    hideOrShowModal(false);
+  };
+
   return (
     <div>
       <Modal
-        children={<Formulario btnText="Editar tarefa" taskList={taskList} />}
+        children={
+          <Formulario
+            btnText="Editar tarefa"
+            taskList={taskList}
+            task={taskToUpdate}
+            handleUpdate={updateTask}
+          />
+        }
       />
       <Header />
       <main className={styles.main}>
@@ -39,7 +73,11 @@ function App() {
         </div>
         <div>
           <h2>Suas tarefas:</h2>
-          <TaskList taskList={taskList} handleDelete={deleteTask} />
+          <TaskList
+            taskList={taskList}
+            handleDelete={deleteTask}
+            handleEdit={editTask}
+          />
         </div>
       </main>
       <Footer />
--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,13 +1,18 @@
 // CSS
 import styles from "./Modal.module.css";
 interface Props {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
-const Modal = ({children}: Props) => {
+const Modal = ({ children }: Props) => {
+  const closeModal = (e: React.MouseEvent): void => {
+    const modal = document.querySelector("modal");
+    modal!.classList.add("hide");
+  };
+
   return (
-    <div id="modal">
-        <div className={styles.fade}></div>
+    <div id="modal" className="hide">
+      <div className={styles.fade} onClick={closeModal}></div>
       <div className={styles.modal}>
         <h2>Texto modal</h2>
         {children}
